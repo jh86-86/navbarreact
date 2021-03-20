976a11b250ff1b95697c2288feef816619694085
JJ <jh_86@hotmail.co.uk>
2021-03-20 12:34:42 +0000

    nav2

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Display from './Component/Display';
 
 function App() {
 
+//sets the state of the pages initially only one page has a normal class
   const[pgOne,setPgOne]=useState("page");
   const[pgTwo,setTwo]=useState("invisible");
   const[pgThree,setThree]=useState("invisible");
 
+
+// a function for each button onClick which changes state of the pages
 function PageOne(){
     setPgOne("page");
     setTwo("invisible");
@@ -30,19 +33,22 @@ function pageThree(){
 
 
 
-
+//an array just to give content to each Display hook,could just make seperate pages
 const pageNumArr=['Home page','page two','page three'];
 
 
   return (
     <div className="App">
-   
+{/*    
+   the navbar is essentially just button wrapped in a div which styles it to look like navbar */}
         <div className="navbar">
           <button  onClick={PageOne}>Home</button>
           <button  onClick={pageTwo}>pageTwo</button>
           <button  onClick={pageThree}>pageThree</button>
         </div>
        
+{/* 
+       each page is a hook of Display but could just be seperate hook pages */}
             <Display state={pgOne} pageNum={pageNumArr[0]} />
             <Display state={pgTwo} pageNum={pageNumArr[1]} />
             <Display state={pgThree} pageNum={pageNumArr[2]} />
